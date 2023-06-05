2d0ab55c8251f4d6fb136be328d0a41ad51db118
izu <a.iz.dnlv.335@gmail.com>
2023-06-05 11:52:16 +0900

    Firestoreでデータ保存

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
@@ -29,16 +30,65 @@ class ChatPage extends StatelessWidget {
                 }),
           ],
         ),
-        body: Center(
-          child: Text('ログイン情報 : ${user.email}'),
+        body: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(8),
+              child: Text('ログイン情報: ${user.email}'),
+            ),
+            Expanded(
+                // FutureBuilder
+                // 非同期処理の結果を元にWidgetを作れる
+                child: FutureBuilder<QuerySnapshot>(
+              // 投稿メッセージ一覧を取得（非同期処理）
+              // 投稿日時でソート
+              future: FirebaseFirestore.instance
+                  .collection('posts')
+                  .orderBy('date')
+                  .get(),
+              builder: (context, snapshot) {
+                // データが取得できた場合
+                if (snapshot.hasData) {
+                  final List<DocumentSnapshot> documents = snapshot.data!.docs;
+                  // 取得した投稿メッセージ一覧を元にリスト表示
+                  return ListView(
+                    children: documents.map((document) {
+                      return Card(
+                          child: ListTile(
+                        title: Text(document['text']),
+                        subtitle: Text(document['email']),
+                        //自分の投稿メッセージの場合は削除ボタンを表示
+                        trailing: document['email'] == user.email
+                            ? IconButton(
+                                icon: Icon(Icons.delete),
+                                onPressed: () async {
+                                  //投稿メッセージのドキュメントを削除
+                                  await FirebaseFirestore.instance
+                                      .collection('posts')
+                                      .doc(document.id)
+                                      .delete();
+                                },
+                              )
+                            : null,
+                      ));
+                    }).toList(),
+                  );
+                }
+                return Center(
+                  child: Text('読込中...'),
+                );
+              },
+            ))
+          ],
         ),
+
         //actionsの外側に置くことで下に来る
         floatingActionButton: FloatingActionButton(
             child: Icon(Icons.add),
             onPressed: () async {
               await Navigator.of(context)
                   .push(MaterialPageRoute(builder: (context) {
-                return AddPostPage();
+                return AddPostPage(user);
               }));
             }));
   }
